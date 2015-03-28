8e93fcfbcf5645dd6055546598dee7a3c3c85f19
Lennart Goedhart <lennart@omnibase.com.au>
2015-03-28 21:13:48 +1100

    Add Webpack to orchestration script
    Looks like it’s a dependency of tidepool-tools/runservers

--- setup_vagrant.sh
+++ setup_vagrant.sh
@@ -10,6 +10,9 @@ npm install --save-dev gulp
 echo "Installing Mocha..."
 npm install -g mocha
 
+echo "Installing Webpack..."
+npm install -g webpack
+
 echo "Installing MongoDB..."
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
